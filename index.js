1bcabf510da16b08f0d84eaa02e1465ad397922e
davebhakti <bhaktidave2206@gmail.com>
2025-07-22 15:42:09 -0700

    Added the middleware and the SQL connection

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 import pg from "pg";
 
+dotenv.config();
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "123456",
+    port: 5432,
+});
+
+db.connect();
+
+//Middlware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
